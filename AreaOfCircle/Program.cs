8b6ecfd05b495358e4a66c509047a8b2d43d3053
jrdotel <juliordotel@gmail.com>
2020-09-29 21:30:01 -0500

    Progress on Studio 1

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -6,7 +6,24 @@ namespace AreaOfCircle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input;
+            double pi;
+            double r;
+            double area;
+            double circumference;
+            double diameter;
+
+            Console.WriteLine("Enter the radius of the circle: ");
+            input = Console.ReadLine();
+            pi = 3.14;
+            r = double.Parse(input);
+            area = (pi * r * r);
+            circumference = (2 * pi * r);
+            diameter = (2 * r);
+
+            Console.WriteLine("The area of a circle of radius " + r + " is: " + area);
+            Console.WriteLine("The circumference of a circle of radius " + r + " is: " + circumference);
+            Console.WriteLine("The are of a diameter of radius " + r + " is: " + diameter);
         }
     }
 }
